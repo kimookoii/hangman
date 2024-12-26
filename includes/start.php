88dc1a8ffc3ddb5424bca84ea8999c360c98de12
zicc <69408905+kimookoii@users.noreply.github.com>
2024-12-26 11:03:42 +0700

    Create  start.php

---
+++ includes/start.php
@@ -0,0 +1,24 @@
+<section class="screen start-screen">
+	<div class="screen-contents">
+		<!-- <form class="word-input" action="#"> -->
+			<h1 class="animate__animated animate__jackInTheBox animate__delay-1s">Hangman!</h1>
+
+			<div class="start-options animate__animated animate__bounceInUp animate__delay-2s">
+				<div class="start-option">
+					<h2>Two Players:</h2>
+					<p>Enter a Word to Start (Hidden).</p>
+					<input class="word" type="text" placeholder="New Word to Guess" maxlength="12">
+					<button type="button" class="start-two-player-game">Start!</button>
+                    <br>
+                    <span id="clear-word-input">clear word input</span>
+				</div>
+
+				<div class="start-option">
+					<h2>One Player:</h2>
+					<button type="button" class="generate-random-word-button">Start with a Random Word!</button>
+				</div>
+			</div>
+
+		<!-- </form> -->
+	</div>
+</section>
