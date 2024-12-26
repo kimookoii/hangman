b33df913703d9f11a0b99a50da8864210812590e
zicc <69408905+kimookoii@users.noreply.github.com>
2024-12-26 11:02:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <?php include 'includes/head.php'; ?>
+    <body>
+        <main>
+            <?php include 'includes/start.php'; ?>
+            <?php include 'includes/game.php'; ?>
+            <?php include 'includes/gameover.php'; ?>
+        </main>
+        <?php include 'includes/footer.php'; ?>
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+        <script src="dist/all.js?cb=1602369644663"></script>
+    </body>
+</html>
