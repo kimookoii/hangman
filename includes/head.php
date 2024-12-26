4025ab1d97371b6f6a77312dfc642ff520a0dc7d
zicc <69408905+kimookoii@users.noreply.github.com>
2024-12-26 11:04:05 +0700

    Create  head.php

---
+++ includes/head.php
@@ -0,0 +1,48 @@
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VYZTC54F01"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-VYZTC54F01');
+    </script>
+
+
+    <!-- Google AdSense -->
+    <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1088778502703155" crossorigin="anonymous"></script> -->
+
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="coolweb.io">
+    <meta name="robots" content="index,follow">
+    <meta name="googlebot" content="index,follow">
+    <meta name="description" content="Play Hangman for Free! One or Two Players. Great vocabulary game for kids to enjoy while learning how to spell new words.">
+    <meta name="keywords" content="hangman, hangman online, hangman game, hangman game online, hangman game online free, hangman free, online games, games for kids, hangman for kids, games, word game for kids, word games">
+
+    <title>Hangman</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Skranji:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="dist/style.css?cb=1602369644663">
+
+    <link rel="apple-touch-icon" sizes="57x57"         href="dist/assets/img/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60"         href="dist/assets/img/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72"         href="dist/assets/img/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76"         href="dist/assets/img/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114"       href="dist/assets/img/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120"       href="dist/assets/img/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144"       href="dist/assets/img/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152"       href="dist/assets/img/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180"       href="dist/assets/img/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="dist/assets/img/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32"    href="dist/assets/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96"    href="dist/assets/img/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16"    href="dist/assets/img/icons/favicon-16x16.png">
+    <link rel="manifest"                               href="dist/assets/img/icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">   
+</head>
